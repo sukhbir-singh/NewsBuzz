90972bb7a4df6fad54ff6701c02cc976e1778551
RamolaWeb <sramola5@gmail.com>
2016-04-02 14:01:52 +0530

    Added a Content Provider , Database and Constant class for storing and fetching data......

---
+++ app/src/main/java/com/newsbuzz/DbContract.java
@@ -0,0 +1,29 @@
+package com.newsbuzz;
+
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class DbContract {
+    public static final String AUTHORITY = "com.newsbuzz";
+    public static final Uri BASE_URI = Uri.parse("content://" + AUTHORITY);
+    public  static final  String TABLE_NEWS="news";
+    public static final String INSERT ="insert" ;
+    public static final String READ ="read" ;
+
+    public static  class  NEWS_TABLE implements BaseColumns{
+        public static final String TITLE="title";
+        public static final String LINK_MORE="link_more";
+        public static final String PUBDATE="pubdate";
+        public static final String DESCRIPTION="description";
+        public static final String LINK_IMAGE="link_image";
+        public static final String CATEGORY="category";
+        public static final String RELATED_LINK="relatedLinks";
+    }
+    public static Uri getNewsByCategory(){
+        return BASE_URI.buildUpon().appendPath(READ).build();
+    }
+    public static Uri insertNews(){
+        return BASE_URI.buildUpon().appendPath(INSERT).build();
+    }
+}
